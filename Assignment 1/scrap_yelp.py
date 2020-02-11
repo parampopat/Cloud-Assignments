a12573e47e708fec3836cc7b34433ffdaea33873
Param Popat <param.popat@gmail.com>
2020-02-10 19:48:39 -0500

    Updated Final Scrapper

--- Assignment 1/scrap_yelp.py
+++ Assignment 1/scrap_yelp.py
@@ -41,8 +41,12 @@ def scrap_yelp(max_offset, cuisine, location):
             businesses = get_business(data)
             for iter in range(len(businesses)):
                 businesses[iter]['cuisine'] = cuisine
-                businesses[iter]['insertedAtTimestamp'] = str(time.time())
-                bus.append(businesses[iter])
+                item = {key: businesses[iter][key] for key in
+                        businesses[iter].keys() & {'id', 'name', 'cuisine', 'rating', 'review_count', 'coordinates',
+                                                   'location'}}
+                for key in item.keys():
+                    item[key] = ' ' if item[key] == '' else str(item[key])
+                bus.append(item)
         except:
             continue
     print("Final Tally for ", keyword, " is ", len(bus))
@@ -51,18 +55,16 @@ def scrap_yelp(max_offset, cuisine, location):
 
 if __name__ == '__main__':
     max_offset = 1000
-    # cuisines = ["chinese", "italian", "indian"]
-    cuisines = ["japanese", "thai", "mexican"]
+    cuisines = ["chinese", "italian", "indian", "japanese", "thai", "mexican"]
     location = 'Manhattan'
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table('yelp-restaurants')
+
+    iterator = 0
     for cuisine in cuisines:
         businesses = scrap_yelp(max_offset=max_offset, cuisine=cuisine, location=location)
-        with table.batch_writer(overwrite_by_pkeys=['insertedAtTimestamp']) as batch:
+        with table.batch_writer() as batch:
             for item in businesses:
-                for key in item.keys():
-                    item[key] = ' ' if item[key] == '' else str(item[key])
-                try:
-                    batch.put_item(Item=item)
-                except:
-                    continue
+                item['insertedAtTimestamp'] = str(iterator)
+                batch.put_item(Item=item)
+                iterator += 1
