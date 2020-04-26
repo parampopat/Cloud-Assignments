945824ba017786ab454609d22601168a957aa891
Param Popat <param.popat@gmail.com>
2020-04-25 22:38:01 -0400

    update s3 url

--- Assignment_3/search_es_photo.py
+++ Assignment_3/search_es_photo.py
@@ -2,16 +2,15 @@ import json
 import os
 from botocore.vendored import requests
 
-
 def lambda_handler(event, context):
     # TODO implement
     print("Event was:", event)
     print("context was", context)
-
+    
     # doc_id = 1
     # file_name = "/tmp/dummy_data.json"
-
-    # dummy_stuff = [
+    
+    # dummy_stuff = [ 
     #     {
     #     "objectKey": "cat-photo1.jpg",
     #     "bucket": "my-photo-bucket",
@@ -29,7 +28,7 @@ def lambda_handler(event, context):
     #     ]
     #     }
     # ]
-
+    
     # with open(file_name, 'w') as outfile:
 
     #     # Load Data into Elastic Search
@@ -47,23 +46,26 @@ def lambda_handler(event, context):
     # cmd = 'curl -XPOST %s --data-binary @%s -H "Content-Type: application/json"' % (es_url, file_name)
     # resp = os.system(cmd)
     # print("Done uploading to Elastic Search Index with res: ", resp)
-
+    
     things = event['body']
     es_url = 'https://vpc-photo-index-6gtjhllhc3y47utn5shxhmfhu4.us-east-1.es.amazonaws.com/photos/_search'
-
+    s3_url = 'https://ppkbphotos.s3.amazonaws.com/'
+    
     photo_list = []
-
+    url_list = []
     for thing in things:
-        query = "{}?q=labels:*{}*".format(es_url, thing)
+        query = "{}?q=labels:*{}*".format( es_url, thing )
         print(query)
         res = requests.get(query).json()['hits']['hits']
         print(res)
-        this_photo_list = [a["_source"]["objectKey"] for a in res]
+        this_photo_list = [ a["_source"]["objectKey"] for a in res]
         photo_list = photo_list + this_photo_list
-
+        url_list = url_list + [ s3_url + item for item in this_photo_list]
+    
+    
     photo_list = list(set(photo_list))
-
+    url_list = list(set(url_list))
     return {
         'statusCode': 200,
-        'body': photo_list
+        'body': [photo_list, url_list]
     }
